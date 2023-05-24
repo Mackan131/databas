215cd6a6e65b4a004ef94569948e41036da2d092
Mackan131 <112383481+Mackan131@users.noreply.github.com>
2023-05-24 12:39:52 +0200

    Add files via upload

---
+++ DbToWFA/WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OleDbConnection conn = new OleDbConnection();
+            conn.ConnectionString = "Provider = Microsoft.ace.oledb.12.0; Data Source = InfoDb.accdb";
+            conn.Open();
+        }
+    }
+}
